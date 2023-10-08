2e43e9530da1b1f9ebd70f05d07093666fde9746
Aubrey Robinson <aubreyrobinson@aubreys-air.ds.usfca.edu>
2023-10-07 22:27:23 -0700

    testing connecting

---
+++ compdetect_client.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+//#include "cJSON.h"
+
+
+#define PORT 8080
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("SERVER_IP");
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Communication code goes here...
+
+    close(sockfd);
+    return 0;
+}
