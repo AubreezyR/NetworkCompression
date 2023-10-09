c3f8827cf825ded8fe6a3e4787b5c5f9f3b1da31
Aubrey Robinson <aubreyrobinson@Aubreys-MacBook-Air.local>
2023-10-09 12:58:06 -0700

    sending UDP packets

--- compdetect_client.c
+++ compdetect_client.c
@@ -9,14 +9,16 @@
 
 #define PORT 8080
 #define MAX_JSON_SIZE 4096
+#define SERVER_IP "192.168.128.3"
+#define PACKET_SIZE 64         // Change to the desired packet size
+#define NUM_PACKETS 10         // Number of packets in each phase
+#define INTER_MEASUREMENT_TIME 5 // Inter-Measurement Time in seconds
 
 int main(int argc, char *argv[]) {
 	if(argc !=2 ){
 		printf("Error: Incorrect amount of args");
 	}
 
-	
-	
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         perror("Socket creation failed");
@@ -27,7 +29,7 @@ int main(int argc, char *argv[]) {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("192.168.128.3");
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("Connection failed");
@@ -35,6 +37,8 @@ int main(int argc, char *argv[]) {
     }
 
     // Communication code goes here...
+
+    //SEND CONFIG///
 	// Read the JSON file into a buffer
     char json_buffer[MAX_JSON_SIZE];
     FILE *json_file = fopen(argv[1], "r");
@@ -52,6 +56,42 @@ int main(int argc, char *argv[]) {
         perror("Error sending JSON data to server");
         close(sockfd);
         return EXIT_FAILURE;
+    }
+
+    //SEND UDP PACKETS//
+
+    char packet[PACKET_SIZE];
+
+    // Phase 1: Send UDP packets with low entropy data
+    printf("Phase 1: Sending UDP packets with low entropy data...\n");
+    for (int i = 0; i < NUM_PACKETS; i++) {
+        // Fill the packet with low entropy data (modify as needed)
+        snprintf(packet, sizeof(packet), "Low Entropy Packet %d", i + 1);
+
+        // Send the packet to the server
+        sendto(sockfd, packet, strlen(packet), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        printf("Sent: %s\n", packet);
+
+        // Add a delay if desired between low entropy packets (e.g., 100 milliseconds)
+        usleep(100000); // Sleep for 100 milliseconds
+    }
+
+    // Inter-Measurement Time (γ)
+    printf("Inter-Measurement Time (γ): Waiting for %d seconds...\n", INTER_MEASUREMENT_TIME);
+    sleep(INTER_MEASUREMENT_TIME);
+
+    // Phase 2: Send UDP packets with high entropy data
+    printf("Phase 2: Sending UDP packets with high entropy data...\n");
+    for (int i = 0; i < NUM_PACKETS; i++) {
+        // Fill the packet with high entropy data (modify as needed)
+        snprintf(packet, sizeof(packet), "High Entropy Packet %d", i + 1);
+
+        // Send the packet to the server
+        sendto(sockfd, packet, strlen(packet), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        printf("Sent: %s\n", packet);
+
+        // Add a delay if desired between high entropy packets (e.g., 100 milliseconds)
+        usleep(100000); // Sleep for 100 milliseconds
     }
 	
 
