9bf0eb232ab5f7450284b6afd8e09fd95a77e886
Aubrey Robinson <aubreyrobinson@aubreys-air.ds.usfca.edu>
2023-10-08 21:39:03 -0700

    m

--- compdetect_client.c
+++ compdetect_client.c
@@ -13,13 +13,29 @@ int main(int argc, char *argv[]) {
 	if(argc !=2 ){
 		printf("Error: Incorrect amount of args");
 	}
-	printf(argv[1]);
+
+	// Open the JSON file for reading
+    FILE *file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Unable to open the JSON file\n");
+        return 1;
+    }
+
+    // Read and print the JSON file contents
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        putchar(c);
+    }
+
+    // Close the file
+    fclose(file);
+	
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
-	FILE *fp 
+	//FILE *fp 
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
@@ -32,7 +48,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Communication code goes here...
-	print("\nconnection successful. Closing socket\n")
+	printf("\nconnection successful. Closing socket\n");
     close(sockfd);
     return 0;
 }
