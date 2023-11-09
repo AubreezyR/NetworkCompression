aface46e9fed6a3be82af6d1cab7867c42043d4c
Aubrey Robinson <aubreyrobinson@Aubreys-MacBook-Air.local>
2023-11-08 22:47:53 -0800

    restar

--- compdetect.c
+++ compdetect.c
@@ -12,6 +12,14 @@
 #include <netinet/tcp.h>
 #include <sys/time.h>
 #include "cJSON.h"
+#define __USE_BSD	/* use bsd'ish ip header */
+#include <sys/socket.h>	/* these headers are for a Linux system, but */
+#include <netinet/in.h>	/* the names on other systems are easy to guess.. */
+#include <netinet/ip.h>
+#define __FAVOR_BSD	/* use bsd'ish tcp header */
+#include <netinet/tcp.h>
+#include <unistd.h>
+
 
 cJSON*json_dict;
 
@@ -87,63 +95,6 @@ void asign_from_json(char* jsonFile) {
     }
 }
 
-/* this function generates header checksums */
-unsigned short 
-csum(unsigned short *buf, int nwords)
-{
-    unsigned long sum;    for (sum = 0; nwords > 0; nwords--)
-        sum += *buf++;
-    sum = (sum >> 16) + (sum & 0xffff);
-    sum += (sum >> 16);
-    return ~sum;
-}
-
-/* This function sets up the ip and tcp header as it set their pointers */
-void setupIPandTCPHeader(struct ipheader *iph, struct tcpheader *tcph, struct sockaddr_in addrSynHead, char *datagram, int destPort)
-{
-	printf("107");
-    iph->ip_hl = 5; // 0101
-    iph->ip_v = 4;  // 0100      combined to make one unsignedchar    01010100
-    iph->ip_tos = 0;
-    iph->ip_len = sizeof(struct ipheader) + sizeof(struct tcphdr); /* no payload */
-    printf("ip len %d",iph->ip_len );
-    iph->ip_id = (unsigned short)htonl(54321);                               /* the value doesn't matter here */
-    iph->ip_off = 0;
-    iph->ip_ttl = 255;
-    iph->ip_p = 6;
-    iph->ip_sum = 0; /* set it to 0 before computing the actual checksum later */
-    iph->ip_src = inet_addr("127.0.0.1"); /* SYN's can be blindly spoofed */
-    iph->ip_dst = addrSynHead.sin_addr.s_addr;
-    tcph->th_sport = htons(1234); /* arbitrary port */
-    tcph->th_dport = htons(destPort);
-    tcph->th_seq = random(); /* in a SYN packet, the sequence is a ra0ndom */
-    tcph->th_ack = 0;        /* number, and the ack sequence is 0 in the 1st packet */
-    tcph->th_x2 = 0;
-    tcph->th_off = 0;            /* first and only tcp segment */
-    tcph->th_flags = TH_SYN;     /* initial connection request */
-    tcph->th_win = (unsigned short)htonl(65535); /* maximum allowed window size */
-    tcph->th_sum = 0;            /* if you set a checksum to zero, your kernel's IP stack
-                            should fill in the correct checksum during transmission */
-    tcph->th_urp = 0;
-
-    iph->ip_sum = csum((unsigned short *)datagram, iph->ip_len >> 1);
-}
-
-void sendSYNPacket(int sockId,struct ipheader *iph, struct tcpheader *tcph, struct sockaddr_in addrSynHead, char *datagram, int destPort){
-  // fill in the tcp and ip header information
-    setupIPandTCPHeader(iph, tcph, addrSynHead, datagram, destPort);
-    // at this point the way the pointers are setup the the ip and tcp header information SHOULD BE IN THE DATAGRAM
-    // send low entropy packet train
-     // send the tcp message
-    if (sendto(sockId, datagram, iph->ip_len, 0, (struct sockaddr *)&addrSynHead, sizeof(addrSynHead)) < 0)
-    {
-        printf("error: could not send TCP Syn Head on raw socket\n");
-    }
-    else
-    {
-        printf("sent TCP Syn to port: %d\n", destPort);
-    }
-}
 
 void send_udp_packets(int payload_type) {
     int s;
@@ -252,77 +203,90 @@ void send_udp_packets(int payload_type) {
 }
 
 
+unsigned short		/* this function generates header checksums */
+csum (unsigned short *buf, int nwords)
+{
+  unsigned long sum;
+  for (sum = 0; nwords > 0; nwords--)
+    sum += *buf++;
+  sum = (sum >> 16) + (sum & 0xffff);
+  sum += (sum >> 16);
+  return ~sum;
+}
+
+void send_syn_packet(){
+	int s = socket (PF_INET, SOCK_RAW, IPPROTO_TCP);	/* open raw socket */
+	  char datagram[4096];	/* this buffer will contain ip header, tcp header,
+				   and payload. we'll point an ip header structure
+				   at its beginning, and a tcp header structure after
+				   that to write the header values into it */
+	  struct ip *iph = (struct ip *) datagram;
+	  struct tcphdr *tcph = (struct tcphdr *) datagram + sizeof (struct ip);
+	  struct sockaddr_in sin;
+				/* the sockaddr_in containing the dest. address is used
+				   in sendto() to determine the datagrams path */
+	
+	  sin.sin_family = AF_INET;
+	  sin.sin_port = htons (9999);/* you byte-order >1byte header values to network
+				      byte order (not needed on big endian machines) */
+	  sin.sin_addr.s_addr = inet_addr ("192.168.128.1");
+	
+	  memset (datagram, 0, 4096);	/* zero out the buffer */
+	
+	/* we'll now fill in the ip/tcp header values, see above for explanations */
+	  iph->ip_hl = 5;
+	  iph->ip_v = 4;
+	  iph->ip_tos = 0;
+	  iph->ip_len = sizeof (struct ip) + sizeof (struct tcphdr);	/* no payload */
+	  iph->ip_id = htonl (54321);	/* the value doesn't matter here */
+	  iph->ip_off = 0;
+	  iph->ip_ttl = 255;
+	  iph->ip_p = 6;
+	  iph->ip_sum = 0;		/* set it to 0 before computing the actual checksum later */
+	  iph->ip_src.s_addr = inet_addr ("1.2.3.4");/* SYN's can be blindly spoofed */
+	  iph->ip_dst.s_addr = sin.sin_addr.s_addr;
+	  tcph->th_sport = htons (1234);	/* arbitrary port */
+	  tcph->th_dport = htons (9999);
+	  tcph->th_seq = random ();/* in a SYN packet, the sequence is a random */
+	  tcph->th_ack = 0;/* number, and the ack sequence is 0 in the 1st packet */
+	  tcph->th_x2 = 0;
+	  tcph->th_off = 0;		/* first and only tcp segment */
+	  tcph->th_flags = TH_SYN;	/* initial connection request */
+	  tcph->th_win = htonl (65535);	/* maximum allowed window size */
+	  tcph->th_sum = 0;/* if you set a checksum to zero, your kernel's IP stack
+			      should fill in the correct checksum during transmission */
+	  tcph->th_urp = 0;
+	
+	  iph->ip_sum = csum ((unsigned short *) datagram, iph->ip_len >> 1);
+	
+	/* finally, it is very advisable to do a IP_HDRINCL call, to make sure
+	   that the kernel knows the header is included in the data, and doesn't
+	   insert its own header into the packet before our data */
+	
+	  {				/* lets do it the ugly way.. */
+	    int one = 1;
+	    const int *val = &one;
+	    if (setsockopt (s, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
+	      printf ("Warning: Cannot set HDRINCL!\n");
+	  }
+	
+	  while (1)
+	    {
+	      if (sendto (s,		/* our socket */
+			  datagram,	/* the buffer containing headers and data */
+			  iph->ip_len,	/* total length of our datagram */
+			  0,		/* routing flags, normally always 0 */
+			  (struct sockaddr *) &sin,	/* socket addr, just like in */
+			  sizeof (sin)) < 0)		/* a normal send() */
+		printf ("error\n");
+	      else
+		printf (".");
+	    }
+	
+	  
+}
+
+
 int main(int argc, char* argv[]){
-	asign_from_json(argv[1]);
-	//--setup socket info and options
-    struct sockaddr_in addrSynHead;
-    // setup SYN head address info
-    addrSynHead.sin_family = PF_INET;
-    addrSynHead.sin_port = htons(cJSON_GetObjectItem(json_dict, "DestinationPortNumberTCPHeadSYN")->valueint);//replace with json
-    addrSynHead.sin_addr.s_addr = inet_addr("192.168.128.1");
-
-    struct sockaddr_in addrSynTail;
-    // setup SYN head address info
-    addrSynTail.sin_family = PF_INET;
-    addrSynTail.sin_port = htons(cJSON_GetObjectItem(json_dict, "DestinationPortNumberTCPTailSYN")->valueint);
-    addrSynTail.sin_addr.s_addr = inet_addr("192.168.128.1");
-
-    //open sockets
-
-    //head
-    int rawSockSYNHead = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-    if (rawSockSYNHead < 0)
-    {
-        printf("Unable to create a socket\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int one = 1;
-    if (setsockopt(rawSockSYNHead, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0)
-    {
-        printf("Warning: Cannot set HDRINCL in head!\n");
-        exit(1); // leave the program
-    }
- 
-    //tail
-    int rawSockSYNTail = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
-    if (rawSockSYNTail < 0)
-    {
-        printf("Unable to create tail socket\n");
-        exit(0);
-    }
-
-    one = 1;
-    if (setsockopt(rawSockSYNTail, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0)
-    {
-        printf("Warning: Cannot set HDRINCL in tail!\n");
-        exit(1); // leave the program
-    }
- 
-    char datagram[4096];
-
-    struct ipheader *iph = (struct ipheader *)datagram;
-    // After the first 20 bytes, comes the tcpheader so point the tcpheader structure to that part
-    // of the datagram
-    struct tcpheader *tcph = (struct tcpheader *)datagram + sizeof(struct ipheader);
-    // make variable to hold SYN head address/port information   
-    
-    memset(datagram, 0, 4096);
-	//TODO setup udp payload send syn adn udps
-
-
-	//-------SEND THE ENTROPY PACKET TRAINS
-	sendSYNPacket(rawSockSYNHead,iph,tcph,addrSynHead,datagram,cJSON_GetObjectItem(json_dict, "DestinationPortNumberTCPHeadSYN")->valueint);
-	clock_t start_time_low, end_time_low,start_time_high, end_time_high;
-	double elapsed_time_low, elapsed_time_high;
-
-	//start_time_low = clock();
-	/*
-	send_udp_packets(0);
-
-	memset(datagram, 0, 4096);
-	sendSYNPacket(rawSockSYNHead,iph,tcph,addrSynTail,datagram,cJSON_GetObjectItem(json_dict, "DestinationPortNumberTCPHeadSYN")->valueint);
-	end_time_low = clock();
-	elapsed_time_low = (double)(end_time_low - start_time_low) / CLOCKS_PER_SEC;
-    */
+	send_syn_packet();
 }
